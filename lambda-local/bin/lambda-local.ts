09f67e546bcd2a73eaba9888d5d7966754fd3140
Alex Parra <jparra@occ.com.mx>
2024-04-23 12:32:39 -0600

    delete lambda-local

--- lambda-local/bin/lambda-local.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib'
-import 'source-map-support/register'
-import { LambdaLocalStack } from '../lib/lambda-local-stack'
-
-const app = new cdk.App()
-new LambdaLocalStack(app, 'LambdaLocalStack', {})
